a2b5d3e10e9b9abe57e039c2f7fed4ce6ca87c87
Antti Ohtonen <anaohtonen@gmail.com>
2023-02-26 21:06:36 +0200

    Modify gui application.

--- gui/guiapplication.py
+++ gui/guiapplication.py
@@ -1,7 +1,8 @@
 """GUI module code."""
-import PySimpleGUI as sg
+from typing import Any
 
-from gui.layouts.general import AbstractLayout
+from gui.gui_backend_interface import GUIBackendInterface
+from gui.layouts.general import BaseLayout, LayoutParam
 from gui.layouts.menu import Menu
 from gui.layouts.maze import Maze
 from gui.layouts.layoutoptions import GUILayout
@@ -10,18 +11,22 @@ from gui.layouts.layoutoptions import GUILayout
 class GUIApplication:  # pylint: disable=too-few-public-methods
     """GUI application."""
 
-    def __init__(self) -> None:
+    def __init__(self, gui_backend_interface: GUIBackendInterface) -> None:
         """Initialize GUI application."""
         self._layouts = {
-            GUILayout.MENU: Menu(),
-            GUILayout.MAZE: Maze(),
+            GUILayout.MENU: Menu(gui_backend_interface),
+            GUILayout.MAZE: Maze(gui_backend_interface),
         }
-        self._current_layout: AbstractLayout = self._layouts[GUILayout.MENU]
+        self._current_layout: BaseLayout = self._layouts[GUILayout.MENU]
 
     def run(self) -> None:
         """Run GUI application."""
+        param: LayoutParam[Any] | None = None
         while True:
-            return_value = self._current_layout.run()
+            return_value = self._current_layout.run(param)
             if return_value.next_layout == GUILayout.CLOSE:
                 break
+            param = LayoutParam(
+                return_value.value
+            )
             self._current_layout = self._layouts[return_value.next_layout]
