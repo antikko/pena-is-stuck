765fcc566a6f80e2ea489cde6523d05a1c01edd9
Antti Ohtonen <anaohtonen@gmail.com>
2023-02-26 12:56:48 +0200

    Create initial main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+"""Main program module."""
+from gui.guiapplication import GUIApplication
+
+
+def main() -> None:
+    """Run program."""
+    gui = GUIApplication()
+    gui.run()
+
+
+if __name__ == "__main__":
+    main()
